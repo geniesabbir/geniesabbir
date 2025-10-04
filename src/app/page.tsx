073ae4cfc964fd39f7f10172d866739b57f70753
Genie Sabbir <genie.sabbir@gmail.com>
2025-10-04 20:46:44 +0600

    feat: add LiquidEther component with WebGL simulation
    - Created LiquidEther component for rendering liquid simulation using WebGL and Three.js.
    - Implemented mouse interaction and auto demo features.
    - Added CSS styles for LiquidEther component.
    - Introduced utility function for class name merging.
    - Configured TypeScript settings for the project.
    - Created main page to display LiquidEther component.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import LiquidEther from '@/components/LiquidEther'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen flex items-center justify-center w-full bg-black'>
+      <LiquidEther />
+    </div>
+  )
+}
+
+export default Home
